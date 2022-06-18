977de4df82d6b696f537fbca855a4becf1d83f68
64x2 / Xofo <79061462+64x2@users.noreply.github.com>
2022-06-18 09:51:11 +0100

    Create farmer.py

---
+++ farmer.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import time
+
+#TOKEN
+TOKEN = "token here"
+
+client = discord.Client()
+b = Bot(command_prefix = ".", self_bot=True)
+b.remove_command("help")
+
+@b.event
+async def on_ready():
+    print("Katheryne bot farmer by xofo")
+    print("For use, type .wish (banner) in any channel \n")
+
+@b.command()
+async def wish(ctx, arg):
+    time.sleep(1)
+    await ctx.send(f"gwish {arg} noani")
+    print("Sent again! Please wait 18 seconds until next command")
+    time.sleep(18)
+    await ctx.send(f".wish {arg}")
+
+b.run(TOKEN, bot = False)
